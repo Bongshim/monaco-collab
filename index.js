262704ea6691c54bf64ad536f0e7070cbb106006
Bongshim <44551557+Bongshim@users.noreply.github.com>
2024-05-18 17:21:22 +0100

    chore: Update room creation API to include users array

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
  */
 app.post('/api/rooms', (req, res) => {
   const { id } = req.body;
-  CodeRoomsState.setRooms([...CodeRoomsState.rooms, { id }]);
+  CodeRoomsState.setRooms([...CodeRoomsState.rooms, { id, users: [] }]);
 
   console.log('CodeRoomsState:', CodeRoomsState.rooms);
   res.status(201).json({
